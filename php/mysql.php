6647cd426cf7593e0ee0d529ac4324d3514b8d34
leechung1226 <85606095+leechung1226@users.noreply.github.com>
2021-06-19 11:50:37 +0800

    Add files via upload

---
+++ php/mysql.php
@@ -0,0 +1,11 @@
+<?php
+/*
+ * @Author: your name
+ * @Date: 2021-06-18 16:41:55
+ * @LastEditTime: 2021-06-18 16:53:27
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \Siemens\php\mysql.php
+ */
+header('content-type:text/html;charset=utf8');
+$link = mysqli_connect('localhost','root','root','html5-2105');
